6cc7091f3c1c641c55a16567fedcbe8f357a2f69
RimaSem <rima.semetaite@gmail.com>
2023-01-10 11:05:48 +0200

    Add temperature type conversion functionality

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 const searchForm = document.querySelector("form");
+const tempConversionBtn = document.querySelector(".temp-type-btn");
+const temperature = document.querySelector(".temperature");
+const temperatureType = document.querySelector(".temperature-type");
+const feelsLikeTemp = document.querySelector(".temp");
+const feelsLikeTempType = document.querySelector(".type");
 
 searchForm.addEventListener("submit", (e) => e.preventDefault());
+
+// temperature conversion
+tempConversionBtn.addEventListener("click", (e) => {
+  if (tempConversionBtn.textContent === "°F") {
+    temperature.textContent = Math.round(convertToF(+temperature.textContent));
+    temperatureType.textContent = "F";
+    feelsLikeTemp.textContent = Math.round(
+      convertToF(+feelsLikeTemp.textContent)
+    );
+    feelsLikeTempType.textContent = "F";
+    tempConversionBtn.textContent = "°C";
+  } else {
+    temperature.textContent = Math.round(convertToC(+temperature.textContent));
+    temperatureType.textContent = "C";
+    feelsLikeTemp.textContent = Math.round(
+      convertToC(+feelsLikeTemp.textContent)
+    );
+    feelsLikeTempType.textContent = "C";
+    tempConversionBtn.textContent = "°F";
+  }
+});
+
+function convertToF(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function convertToC(fahrenheit) {
+  return (fahrenheit - 32) * (5 / 9);
+}
