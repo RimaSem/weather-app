57b7663708ad1274fdb03b54d893cf34762cbf1f
RimaSem <rima.semetaite@gmail.com>
2023-01-10 13:22:04 +0200

    Fetch and display weather data

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
+const APIkey = "13e50692f650e4663cbb5ad7302e4d31";
 const searchForm = document.querySelector("form");
+const userInput = document.querySelector("form input");
 const tempConversionBtn = document.querySelector(".temp-type-btn");
+const errorDisplay = document.querySelector(".error-message");
+
+const cityName = document.querySelector(".location");
+const weatherIcon = document.querySelector(".weather-icon img");
 const temperature = document.querySelector(".temperature");
 const temperatureType = document.querySelector(".temperature-type");
+const weatherDescription = document.querySelector(".weather-description");
+
 const feelsLikeTemp = document.querySelector(".temp");
 const feelsLikeTempType = document.querySelector(".type");
+const humidity = document.querySelector(".humidity .data");
+const rainChance = document.querySelector(".rain-chance .data");
+const windSpeed = document.querySelector(".wind .data");
 
-searchForm.addEventListener("submit", (e) => e.preventDefault());
+searchForm.addEventListener("submit", (e) => {
+  errorDisplay.style.display = "none";
+  e.preventDefault();
+  getWeather(userInput.value).catch((err) => {
+    errorDisplay.style.display = "block";
+    console.log(err.message);
+  });
+});
+
+async function getWeather(city) {
+  const cityResponse = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`
+  );
+  const cityData = await cityResponse.json();
+  cityName.textContent = `${cityData[0].name}, ${cityData[0].country}`;
+
+  const weatherResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${cityData[0].lat}&lon=${cityData[0].lon}&appid=${APIkey}&units=metric`
+  );
+  const weatherData = await weatherResponse.json();
+  console.log(weatherData);
+  //   weatherIcon.src = weatherData.weather[0].icon;
+  temperature.textContent = Math.round(+weatherData.list[0].main.temp);
+  temperatureType.textContent = "C";
+  weatherDescription.textContent = weatherData.list[0].weather[0].main;
+  feelsLikeTemp.textContent = Math.round(+weatherData.list[0].main.feels_like);
+  feelsLikeTempType.textContent = "C";
+  humidity.textContent = `${weatherData.list[0].main.humidity}%`;
+  rainChance.textContent = `${+weatherData.list[0].pop * 100}%`;
+  windSpeed.textContent = `${Math.round(weatherData.list[0].wind.speed)} m/s`;
+
+  tempConversionBtn.textContent = "°F";
+}
 
 // temperature conversion
 tempConversionBtn.addEventListener("click", (e) => {
